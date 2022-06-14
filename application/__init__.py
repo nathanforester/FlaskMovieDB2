79048a1f2fc4561055fc2f3e7fceebf87d53f53d
Nathan Forester <Nathan.Forester@estio.co.uk>
2022-06-14 20:12:43 +0100

    first commit

---
+++ application/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+url = 'mysql+pymysql://' + os.environ['USERNAME'] + ':' + os.envrion['PASSWORD'] + '@' + os.environ['DB_ENDPOINT'] + ':' + os.environ['PORT'] + '/' + os.environ['DB_NAME']
+app.config['SQLALCHEMY_DATABASE_URI'] = url
+app.config['SECRET_KEY'] = '123456789'
+
+db = SQLAlchemy(app)
+
+engine= create_engine(url, echo=True)
+if not database_exists(engine.url):
+    create_database(engine.url)
+else:
+    engine.connect()
+
+import application.routes 
---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+sudo apt update
+sudo apt-get install python3-venv -y
+sudo apt install mysql-server -y
+
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+python3 create.py
+
+
