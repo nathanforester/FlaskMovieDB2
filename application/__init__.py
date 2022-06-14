3cac0d84054b4b840658f51e2adeca4785aa9e02
Nathan Forester <Nathan.Forester@estio.co.uk>
2022-06-14 20:21:23 +0100

    no port value needed

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ from sqlalchemy_utils import database_exists, create_database
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-url = 'mysql+pymysql://' + os.environ['USERNAME'] + ':' + os.envrion['PASSWORD'] + '@' + os.environ['DB_ENDPOINT'] + ':' + os.environ['PORT'] + '/' + os.environ['DB_NAME']
+url = 'mysql+pymysql://' + os.environ['USERNAME'] + ':' + os.envrion['PASSWORD'] + '@' + os.environ['DB_ENDPOINT'] + '/' + os.environ['DB_NAME']
 app.config['SQLALCHEMY_DATABASE_URI'] = url
 app.config['SECRET_KEY'] = '123456789'
 
