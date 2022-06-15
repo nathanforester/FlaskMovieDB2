29a3331909158d8ee8127ec76f53bb5fbe1aff9a
nathanforester <nfeugene86@gmail.com>
2022-06-15 02:15:54 +0100

    changed script.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt-get install python3-venv -y
 
-pip3 install -r /home/ubuntu/FlaskMovieDB2/requirements.txt
+pip install -r /home/ubuntu/FlaskMovieDB2/requirements.txt
 
 python3 /home/ubuntu/FlaskMovieDB2/create.py
 
