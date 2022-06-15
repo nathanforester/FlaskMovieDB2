be75a590e7c36159666e8aac4697af9dbb9c99ab
nathanforester <nfeugene86@gmail.com>
2022-06-15 02:05:53 +0100

    changed script.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,7 @@
 
 sudo apt update
 sudo apt-get install python3-venv -y
-sudo apt install mysql-server -y
 
-python3 -m venv venv
-source venv/bin/activate
 pip3 install -r /home/ubuntu/FlaskMovieDB2/requirements.txt
 
 python3 /home/ubuntu/FlaskMovieDB2/create.py
