67099b6401788ee23736ec9f71db1a4337884408
nathanforester <nfeugene86@gmail.com>
2022-06-15 11:25:04 +0100

    changed script.sh

--- startup.sh
+++ startup.sh
@@ -6,10 +6,6 @@ sudo apt install python3-pip -y
 
 pip install -r /home/ubuntu/FlaskMovieDB2/requirements.txt
 
-python -m pip uninstall flask-sqlalchemy
-
-python -m pip install flask-sqlalchemy
-
 python3 /home/ubuntu/FlaskMovieDB2/create.py
 
 
