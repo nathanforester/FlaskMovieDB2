e2b437d0541a03731237281522375253942d3429
nathanforester <nfeugene86@gmail.com>
2022-12-20 15:06:03 +0000

    ready for automation

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,6 @@ from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
 username = os.environ.get('USERNAME')
 password = os.environ.get('PASSWORD')
@@ -13,12 +12,13 @@ endpoint  = os.environ.get('ENDPOINT')
 name     = os.environ.get('NAME')
 
 #url = 'sqlite:///C:\\Users\\nathan.forester\\Documents\\movies.db' 
-# url = f'mysql+pymysql://{username}:{password}@{endpoint}/{name}'
-url = 'mysql+pymysql://nathan:password@my-rds-27-09.cb9iyz332tc1.eu-west-2.rds.amazonaws.com:3306/Here'
+url = f'mysql+pymysql://{username}:{password}@{endpoint}/{name}'
+#url = 'mysql+pymysql://nathan:password@my-rds-27-09.cb9iyz332tc1.eu-west-2.rds.amazonaws.com:3306/Here'
 app.config['SQLALCHEMY_DATABASE_URI'] = url
 app.config['SECRET_KEY'] = '123456789'
 
 db = SQLAlchemy(app)
+app.app_context().push()
 
 engine= create_engine(url, echo=True)
 if not database_exists(engine.url):
