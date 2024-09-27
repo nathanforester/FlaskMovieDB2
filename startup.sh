657149f9df7a5fc1283d762952eb135eb0411d10
Nathan Forester <49757908+nathanforester@users.noreply.github.com>
2024-09-27 12:47:29 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash 
 
 # docker-compose -f /home/ubuntu/FlaskMovieDB2/docker-compose.yaml up -d
-python3 -m pip install /home/ubuntu/FlaskMovieDB2/requirements.txt
+# python3 -m pip install /home/ubuntu/FlaskMovieDB2/requirements.txt
+
+sudo apt install python3-flask -y
+sudo apt install python3-Flask-SQLAlchemy -y
+sudo apt install python3-Flask-WTF -y
+sudo apt install python3-Werkzeug -y
+sudo apt install python3-itsdangerous -y
+sudo apt install python3-Jinja2 -y
+sudo apt install python3-MarkupSafe -y
+sudo apt install python3-SQLAlchemy -y
+sudo apt install python3-WTForms -y
+sudo apt install python3-pymysql -y
+sudo apt install python3-sqlalchemy_utils -y
+sudo apt install python3-pytest -y
+sudo apt install python3-Flask-Testing -y
+sudo apt install python3-flask_bcrypt -y
 
 
 
