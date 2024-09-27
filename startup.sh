66676e7be6a60e3045b126af51e4870840657a4e
Nathan Forester <49757908+nathanforester@users.noreply.github.com>
2024-09-27 12:02:06 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-docker-compose -f /home/ubuntu/FlaskMovieDB2/docker-compose.yaml up -d
+# docker-compose -f /home/ubuntu/FlaskMovieDB2/docker-compose.yaml up -d
+python3 -m pip install /home/ubuntu/FlaskMovieDB2/requirements.txt
 
 
 
